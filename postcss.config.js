64399b913d4cd42bc27c5b2e06d8cdf8ab7ea19b
Rogerio Leal <rogerioleal1@gmail.com>
2022-06-20 10:00:04 -0300

    refactor: Instalando purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,21 @@
 module.exports = {
-    "plugins": [
+    plugins: [
       "postcss-flexbugs-fixes",
       [
         "postcss-preset-env",
         {
-          "autoprefixer": {
-            "flexbox": "no-2009"
-          },
-          "stage": 3,
-          "features": {
-            "custom-properties": false
-          }
+          autoprefixer: { flexbox: "no-2009" },
+          stage: 3,
+          features: { "custom-properties": false }
         }
       ],
       [
-        '@fullhuman/postcss-purgecss',
+        "@fullhuman/postcss-purgecss",
         {
-          content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}'
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          content: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
           safelist: ["html", "body"]
         }
-      ],
+      ]
     ]
-  }
+  };
